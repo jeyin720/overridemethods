6a3136d9ad0eaddd0ccd3dace2c60ae3253de4d9
jeyin <jeyin@DESKTOP-DO85QHL>
2020-06-02 10:26:17 +0900

    override method

---
+++ overridemethods/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace overridemethods
+{
+    class Program
+    {
+        int Power(int input)
+        {
+            return Power(input);
+        }
+        int Power(int input, int count)
+        {
+            return Power(input, count);
+        }
+        int SumAll(int end)
+        {
+            return SumAll(end);
+        }
+        int SumAll(int start, int end)
+        {
+            return SumAll(start, end);
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
